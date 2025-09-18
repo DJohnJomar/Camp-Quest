087daf2324c63bb000dbccea7707cea53e51960f
John Jomar Dimaunahan <johndjomar@gmail.com>
2025-09-18 17:50:05 +0800

    feat: Add CRUD functionality
    add method-override middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 
 main()
@@ -23,6 +25,49 @@ async function main() {
 app.get('/', (req, res) => {
   res.render('home');
 });
+
+//Index, home show
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find();
+  res.render('campgrounds/index', {campgrounds});
+});
+
+//Creating new
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+})
+
+//Show
+app.get('/campgrounds/:id', async (req, res) => {
+  const {id} = req.params;
+  const campground = await Campground.findById(id);
+  res.render('campgrounds/show', {campground});
+});
+
+//Updating
+app.patch('/campgrounds/:id', async(req, res) => {
+  const campground = await Campground.findByIdAndUpdate(req.params.id, req.body.campground, {new:true, runValidators:true});
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+//Edit show
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/edit', {campground});
+})
+
+//Delete 
+app.delete('/campgrounds/:id', async (req, res) => {
+  await Campground.findByIdAndDelete(req.params.id);
+  res.redirect('/campgrounds');
+})
+
 
 
 app.listen(3000, () => {

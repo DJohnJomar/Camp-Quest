50b04906acbdea145a9b6a863d165b991bd032fc
John Jomar Dimaunahan <johndjomar@gmail.com>
2025-09-19 21:39:45 +0800

    add image and description fields to camp model and display

--- seeds/index.js
+++ seeds/index.js
@@ -4,10 +4,10 @@ const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 main()
-.then(() => {
-  console.log('Connected to DB!')
-})
-.catch(err => console.log(err));
+  .then(() => {
+    console.log('Connected to DB!')
+  })
+  .catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/camp-quest');
 };
@@ -16,17 +16,22 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
-        await camp.save();
-    }
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto tenetur laudantium labore cupiditate, optio, illum, asperiores quisquam tempora in quam corporis neque! Nesciunt nostrum eveniet ipsa, molestias alias labore laborum.',
+      price 
+    });
+    await camp.save();
+  }
 }
 
 seedDB().then(() => {
-    mongoose.connection.close();
+  mongoose.connection.close();
 })

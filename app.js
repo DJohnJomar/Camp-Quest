554063a6d7e6d784449d8b5bc92c42b6c753ad43
John Jomar Dimaunahan <johndjomar@gmail.com>
2025-10-28 12:42:24 +0800

    chore: Create public folder to serve static assets

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(morgan('tiny')); //Logs requests (method, url, status code,  size, response size, response time)

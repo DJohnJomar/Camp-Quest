38076ceed44ccaf2e328bec6d8cb88b31a5d7505
John Jomar Dimaunahan <johndjomar@gmail.com>
2025-10-31 17:43:54 +0800

    fix: Add missing parenthesis in User.deseralizeUser()

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser);
+passport.deserializeUser(User.deserializeUser());
 
 //Routes
 app.use('/campgrounds', campgroundRoutes);

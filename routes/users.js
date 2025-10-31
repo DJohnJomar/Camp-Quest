c227c529669c4a11f7fe761590af06741940351f
John Jomar Dimaunahan <johndjomar@gmail.com>
2025-10-31 17:24:50 +0800

    feat: Finish Register Route Logic

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,21 @@ const router = express.Router();
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
-  res.render('users/register');
+    res.render('users/register');
 });
 
 router.post('/register', async (req, res) => {
-  res.send(req.body);
+    try {
+        const { username, email, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to Camp Quest!');
+        res.redirect('/campgrounds');
+    } catch (error) {
+        req.flash('error', error.message);
+        res.redirect('/register');
+    }
+
 })
 
 module.exports = router;

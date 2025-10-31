be85aeeb3d60af6861129a46e7871e513b742f41
John Jomar Dimaunahan <johndjomar@gmail.com>
2025-10-31 16:56:01 +0800

    feat: Set up Passport

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const campgroundRoutes = require('./routes/campgrounds.js');
 const reviewRoutes = require('./routes/reviews.js');
@@ -42,6 +45,13 @@ app.use((req, res, next) => {
   // res.locals.message = req.flash('failure');
   next();
 })
+
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser);
 
 //Routes
 app.use('/campgrounds', campgroundRoutes);

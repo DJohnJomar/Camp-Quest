f143aac684c9791722f6fad1f4b2ec332fd57056
John Jomar Dimaunahan <johndjomar@gmail.com>
2025-11-04 13:56:34 +0800

    feat: Add isReviewAuthor middleware to reviews - Prevent unauthorized deletion of reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,7 +4,7 @@ const {reviewJoiSchema} = require('../joiSchemas');
 const Review = require('../models/review');
 const Campground = require('../models/campground');
 const ExpressError = require('../utils/ExpressError');
-const {isLoggedIn, validateReview} = require('../middleware.js');
+const {isLoggedIn, validateReview, isReviewAuthor} = require('../middleware.js');
 
 //New Review
 router.post('/', isLoggedIn, validateReview, async (req, res) => {
@@ -20,7 +20,7 @@ router.post('/', isLoggedIn, validateReview, async (req, res) => {
 });
 
 //Delete Review
-router.delete('/:reviewId', isLoggedIn, async (req, res) => {
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, async (req, res) => {
   const {id, reviewId} = req.params;
   await Campground.findByIdAndUpdate(id, {$pull:{reviews:reviewId}}); //pull removes from an array matching a condition.
   await Review.findByIdAndDelete(reviewId);
